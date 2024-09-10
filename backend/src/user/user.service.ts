20a50bb9e50ef7379a4083f225332f9b0f1de1a8
alexander <alexanderjuror@gmail.com>
2024-09-10 22:39:20 +0300

    added types for services

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,5 +1,24 @@
 
 import { body, validationResult } from "express-validator";
 
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    banks: string[];
+}
+
+type BankAccount = {
+    name: string;
+    userId: number;
+    cards: string[];
+}
+
+type Card = {
+    name: string;
+    bankAccountId: number;
+}
+
 import * as UserService from './user.service';
 
